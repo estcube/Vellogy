620ca03afe0d680fcf395b1646949346b9d4b0ff
Annika Jaakson <annika.jaakson@estcube.eu>
2020-11-11 19:08:15 +0200

    Add gitignore
    ECIIOBC-1056

--- build/obj/logging.d
+++
@@ -1,4 +0,0 @@
-data_logging/build/obj/logging.o: data_logging/src/logging.cpp \
- /home/user-admin/ESTCube/firmware/data_logging/inc/logging.h
-
-/home/user-admin/ESTCube/firmware/data_logging/inc/logging.h:
