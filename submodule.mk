6c80e5ef50cfdca592bb05b47054652a6670f876
Kerdo Kurs <kerdo.kurs@estcube.eu>
2022-01-25 14:02:19 +0200

    Fix submodule.mk
    ECIIOBC-1056

--- submodule.mk
+++ submodule.mk
@@ -1,15 +1,15 @@
-ifndef data_logging_mk
-data_logging_mk := Prevent repeated "-include".
+ifndef app_logging_mk
+app_logging_mk := Prevent repeated "-include".
 
-data_logging.SRC = $(wildcard $(data_logging)src/*.cpp)
-data_logging.OBJ = $(patsubst $(data_logging)src/%.cpp, $(data_logging)build/obj/%.o, $(data_logging.SRC))
+app_logging.SRC = $(wildcard $(app_logging)src/*.cpp)
+app_logging.OBJ = $(patsubst $(app_logging)src/%.cpp, $(app_logging)build/obj/%.o, $(app_logging.SRC))
 
-$(data_logging)build/liblogging.a: $(data_logging)submodule.mk $(data_logging) $(data_logging.OBJ)
+$(app_logging)build/liblogging.a: $(app_logging)submodule.mk $(app_logging) $(app_logging.OBJ)
 	$(DIR_GUARD)
-	@$(AR) rcs $@ $(data_logging.OBJ)
+	@$(AR) rcs $@ $(app_logging.OBJ)
 	@if tty -s; then tput setaf 2; echo "Data logging complete"; tput sgr0; fi
 
-$(data_logging)build/obj/%.o: $(data_logging)submodule.mk $(data_logging) $(data_logging)src/%.cpp bal/build/inc/ecbal.h
+$(app_logging)build/obj/%.o: $(app_logging)submodule.mk $(app_logging) $(app_logging)src/%.cpp bal/build/inc/ecbal.h
 	$(DIR_GUARD)
 	@$(CPPC) $(CPPFLAGS) -DBOARD_$(BOARD) -o $@ $(word 3,$^)
 
